0ec37ae217cb6f397dd4527cc367fae648593ab4
3sulton <16520492@std.stei.itb.ac.id>
2022-04-01 11:22:38 +0700

    add welcome and choose puzzle from random or file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,20 @@
 import ReadFileConfiguration as rfc
 
-puzzles = rfc.ReadFileConfiguration().puzzles
-print(puzzles)
+print("Welcome to the 15-Puzzle Solver Program by: Tri Sulton Adila (13520033)")
+print("Please enter the method to generate puzzle:")
+print("1. Random")
+print("2. Manual")
+generate_puzzle = int(input("Your choice: "))
+
+if(generate_puzzle == 1):
+    print("not done")
+elif(generate_puzzle == 2):
+    puzzles = rfc.ReadFileConfiguration().puzzles
+    print(puzzles)
+
+
+# for puzzle in puzzles:
+    # if bisa diselesaikan
+        # solve(puzzle)
+    # else:
+        # print("Puzzle cannot be solved")
