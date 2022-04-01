ef296113b9d51aa14fa8692072b21d8c4d3f86bd
3sulton <16520492@std.stei.itb.ac.id>
2022-04-01 11:23:41 +0700

    change some description of method

--- src/ReadFileConfiguration.py
+++ src/ReadFileConfiguration.py
@@ -8,23 +8,23 @@ class ReadFileConfiguration:
     def get_all_files_in_directory(self, directory):
         """
         This method returns a list of all files in a directory
-        :param directory:
-        :return:
+        :param directory: string
+        :return: file_name: list of string
         """
         with os.scandir(directory) as files:
             return [file.name for file in files if file.is_file()]
 
     def read_puzzle(self, file_name):
         """
-        This method reads a file and returns the content of the file
-        :param file_name:
-        :return:
+        This method reads a file and returns the puzzle
+        :param file_name: string
+        :return: puzzle: list of list of char
         """
         puzzle = []
         with open(self.directory_name + "/" + file_name, 'r') as f:
             data = f.read()
             for line in data.splitlines():
-                # get rid of whitespace)
+                # get rid of whitespace
                 row = [number for number in line.split()]
                 puzzle.append(row)
         return puzzle
@@ -32,9 +32,9 @@ class ReadFileConfiguration:
     def get_puzzles(self):
         """
         This method returns a list of all puzzle files
-        :return:
+        :return: list of puzzle
         """
-        return [self.read_puzzle(file) for file in self.get_all_files_in_directory(self.directory_name)]
+        return [self.read_puzzle(file_name) for file_name in self.get_all_files_in_directory(self.directory_name)]
     
 
     
