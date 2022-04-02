204060658e78cdd207cb5845ca8d68561350b2ef
3sulton <16520492@std.stei.itb.ac.id>
2022-04-02 11:15:59 +0700

    add cetak puzzle

--- src/Puzzle.py
+++ src/Puzzle.py
@@ -1,6 +1,7 @@
 class Puzzle:
     def __init__(self, puzzle):
         self.puzzle = puzzle # list of list of char
+        self.cost = 0 # int
 
     def is_solveable(self):
         """
@@ -26,17 +27,9 @@ class Puzzle:
         linear_puzzle = [int(number) for row in self.puzzle for number in row if number != 'x']
         count = 0
         for i in range(15):
-            # ctr = 0
             for j in range(i + 1, 15):
                 if linear_puzzle[i] > linear_puzzle[j]:
-                    # ctr += 1
                     count += 1
-            # if(i == 14):
-            #     for j in range(14):
-            #         if linear_puzzle[i] > linear_puzzle[j]:
-            #             count += 1
-                        # ctr+= 1
-            #print([i, ctr])
         return count
 
     def get_x_row(self):
@@ -48,10 +41,22 @@ class Puzzle:
             if 'x' in col:
                 return row
 
-    def solve(self):
+    def cetak(self):
         """
-        This method solves the puzzle
-        :return: list of list of char
+        This method prints the puzzle
+        :return: None
         """
-        print("hiya hiya hiya")
-
+        for i in range(4):
+            print("+----" * 4 + "+")
+            for j in range(4):
+                number = self.puzzle[i][j]
+                if number == 'x':
+                    print("|    ", end="")
+                else:
+                    if len(number) == 1:
+                        print("| 0{} ".format(self.puzzle[i][j]), end="")
+                    else:
+                        print("| {} ".format(self.puzzle[i][j]), end="")
+                if(j == 3):
+                    print("|")
+        print("+----" * 4 + "+")
